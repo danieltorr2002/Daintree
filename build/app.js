6d6b0b8a90686a09db4be1935c1a68dd5728477e
dtorres <Danieltorr2002@gmail.com>
2018-12-10 21:50:02 -0500

    d

--- build/app.js
+++ build/app.js
@@ -108,7 +108,6 @@ function showUseless(){
     counter.style.background = "white";
     back.style.display = "inherit";
     back.style.background = "none";
-    back.style.background = "white";
 }
 
 function count(){
